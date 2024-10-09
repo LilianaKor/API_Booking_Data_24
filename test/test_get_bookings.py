5ecc6f31def745a1cb8e3dd42c7bea6bfa7bb68c
Liliana <lilianakoric@gmail.com>
2024-10-08 21:42:13 -0700

    test_get_booking - pass, and was added

--- test/test_get_bookings.py
+++ test/test_get_bookings.py
@@ -6,7 +6,6 @@ from src.assertions import assertions
 URL = "https://restful-booker.herokuapp.com/booking"
 
 
-
 def test_get_token():
     response = requests.get(URL)
     assertions.assert_status_code(response, HTTPStatus.OK)
